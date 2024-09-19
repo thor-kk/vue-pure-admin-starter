1312e2f40d6857f652bae21fe5b8be74f2cc3293
thor.kk <1317290619@qq.com>
2024-09-19 20:19:25 +0800

    init: 初始化 高级表单

---
+++ src/components/ProForm/index.ts
@@ -0,0 +1,5 @@
+import type ProForm from './index.vue'
+
+export declare type ProFormInstance = InstanceType<typeof ProForm>
+
+export { default as ProForm } from './index.vue'
--- src/views/echarts/demo2.vue
+++ src/views/echarts/demo2.vue
@@ -1,16 +1,17 @@
 <!--
  * @Author: Yyy
  * @Date: 2024-09-10 16:49:37
- * @LastEditTime: 2024-09-10 16:59:06
+ * @LastEditTime: 2024-09-19 20:18:06
  * @Description: 图表 - 示例二
 -->
 
 <script setup lang="ts">
-defineOptions({ name: "echarts-demo2" });
+import { ProForm, ProFormInstance } from '@/components'
+defineOptions({ name: 'echarts-demo2' })
 </script>
 
 <template>
-  <div>echarts-demo2</div>
+  <div><ProForm /></div>
 </template>
 
 <style scoped lang="scss"></style>
