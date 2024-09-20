8b07a17dee98f7f36ae4ec44febfff3b6dfc2d0b
thor.kk <1317290619@qq.com>
2024-09-20 15:53:56 +0800

    feat: 集成 PlusProComponents

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { ElConfigProvider } from "element-plus";
-import { ReDialog } from "@/components/ReDialog";
-import zhCn from "element-plus/es/locale/lang/zh-cn";
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+import { ReDialog } from '@/components/ReDialog'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import plusZhCn from 'plus-pro-components/es/locale/lang/zh-cn'
 
 export default defineComponent({
-  name: "app",
+  name: 'app',
   components: {
     [ElConfigProvider.name]: ElConfigProvider,
     ReDialog
   },
   computed: {
     currentLocale() {
-      return zhCn;
+      return {
+        ...zhCn,
+        ...plusZhCn
+      }
     }
   }
-});
+})
 </script>
--- src/views/form/search/index.vue
+++ src/views/form/search/index.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { PlusSearch } from 'plus-pro-components'
+import { columns } from './data'
+
 defineOptions({ name: 'page-form-plus-search' })
+
+const state = ref({
+  status: '0',
+  time: new Date().toString()
+})
+
+const handleChange = (values: any) => {
+  console.log(values, 'change')
+}
+const handleSearch = (values: any) => {
+  console.log(values, 'search')
+}
+const handleReset = () => {
+  console.log('handleReset')
+}
 </script>
 
 <template>
-  <div>1</div>
+  <div>
+    <el-card shadow="never">
+      <PlusSearch
+        v-model="state"
+        :columns="columns"
+        :show-number="2"
+        :collapse-transition="false"
+        @change="handleChange"
+        @search="handleSearch"
+        @reset="handleReset"
+      />
+    </el-card>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
