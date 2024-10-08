e33d763a58396152824a596fdab1ec99fdd29534
thor.kk <1317290619@qq.com>
2024-10-08 14:22:11 +0800

    feat: 梳理 接口模块

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,45 +1,45 @@
-import { http } from "@/utils/http";
+import { http } from '@/utils/http'
 
 export type UserResult = {
-  success: boolean;
+  success: boolean
   data: {
     /** 头像 */
-    avatar: string;
+    avatar: string
     /** 用户名 */
-    username: string;
+    username: string
     /** 昵称 */
-    nickname: string;
+    nickname: string
     /** 当前登录用户的角色 */
-    roles: Array<string>;
+    roles: Array<string>
     /** 按钮级别权限 */
-    permissions: Array<string>;
+    permissions: Array<string>
     /** `token` */
-    accessToken: string;
+    accessToken: string
     /** 用于调用刷新`accessToken`的接口时所需的`token` */
-    refreshToken: string;
+    refreshToken: string
     /** `accessToken`的过期时间（格式'xxxx/xx/xx xx:xx:xx'） */
-    expires: Date;
-  };
-};
+    expires: Date
+  }
+}
 
 export type RefreshTokenResult = {
-  success: boolean;
+  success: boolean
   data: {
     /** `token` */
-    accessToken: string;
+    accessToken: string
     /** 用于调用刷新`accessToken`的接口时所需的`token` */
-    refreshToken: string;
+    refreshToken: string
     /** `accessToken`的过期时间（格式'xxxx/xx/xx xx:xx:xx'） */
-    expires: Date;
-  };
-};
+    expires: Date
+  }
+}
 
 /** 登录 */
 export const getLogin = (data?: object) => {
-  return http.request<UserResult>("post", "/login", { data });
-};
+  return http.request<UserResult>('post', '/login', { data })
+}
 
 /** 刷新`token` */
 export const refreshTokenApi = (data?: object) => {
-  return http.request<RefreshTokenResult>("post", "/refresh-token", { data });
-};
+  return http.request<RefreshTokenResult>('post', '/refresh-token', { data })
+}
