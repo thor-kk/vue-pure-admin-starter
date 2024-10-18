51415c2b63ec186adeda723c239bec86025ae26c
thor.kk <1317290619@qq.com>
2024-10-18 15:36:27 +0800

    feat(系统监控): 迁移 登录日志页

--- src/router/modules/monitor.ts
+++ src/router/modules/monitor.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Yyy
  * @Date: 2024-09-10 16:46:30
- * @LastEditTime: 2024-10-18 15:04:15
+ * @LastEditTime: 2024-10-18 15:33:56
  * @Description: router - 系统监控
  */
 
@@ -12,8 +12,14 @@ export default {
     {
       path: '/monitor/online',
       name: 'page-monitor-online',
-      meta: { title: '在线用户', showParent: true },
+      meta: { title: '在线用户' },
       component: () => import('@/views/monitor/online/index.vue')
+    },
+    {
+      path: '/monitor/logs/login',
+      name: 'page-monitor-logs-login',
+      meta: { title: '登录日志' },
+      component: () => import('@/views/monitor/logs/login/index.vue')
     }
   ]
 } satisfies RouteConfigsTable
