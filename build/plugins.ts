1cf0f86f974292ca2bd45d956d5acb0714da182b
thor.kk <1317290619@qq.com>
2024-12-01 21:48:02 +0800

    chore: vite-plugin-vue-devtools

--- build/plugins.ts
+++ build/plugins.ts
@@ -1,34 +1,33 @@
-import { cdn } from "./cdn";
-import vue from "@vitejs/plugin-vue";
-import { pathResolve } from "./utils";
-import { viteBuildInfo } from "./info";
-import svgLoader from "vite-svg-loader";
-import type { PluginOption } from "vite";
-import checker from "vite-plugin-checker";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import Inspector from "vite-plugin-vue-inspector";
-import { configCompressPlugin } from "./compress";
-import removeNoMatch from "vite-plugin-router-warn";
-import { visualizer } from "rollup-plugin-visualizer";
-import removeConsole from "vite-plugin-remove-console";
-import { themePreprocessorPlugin } from "@pureadmin/theme";
-import { genScssMultipleScopeVars } from "../src/layout/theme";
-import { vitePluginFakeServer } from "vite-plugin-fake-server";
+import { cdn } from './cdn'
+import vue from '@vitejs/plugin-vue'
+import { pathResolve } from './utils'
+import { viteBuildInfo } from './info'
+import svgLoader from 'vite-svg-loader'
+import type { PluginOption } from 'vite'
+import checker from 'vite-plugin-checker'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import Inspector from 'vite-plugin-vue-inspector'
+import { configCompressPlugin } from './compress'
+import removeNoMatch from 'vite-plugin-router-warn'
+import { visualizer } from 'rollup-plugin-visualizer'
+import removeConsole from 'vite-plugin-remove-console'
+import { themePreprocessorPlugin } from '@pureadmin/theme'
+import { genScssMultipleScopeVars } from '../src/layout/theme'
+import { vitePluginFakeServer } from 'vite-plugin-fake-server'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-export function getPluginsList(
-  VITE_CDN: boolean,
-  VITE_COMPRESSION: ViteCompression
-): PluginOption[] {
-  const lifecycle = process.env.npm_lifecycle_event;
+export function getPluginsList(VITE_CDN: boolean, VITE_COMPRESSION: ViteCompression): PluginOption[] {
+  const lifecycle = process.env.npm_lifecycle_event
   return [
     vue(),
     // jsx、tsx语法支持
     vueJsx(),
+    vueDevTools(),
     checker({
       typescript: true,
       vueTsc: true,
       eslint: {
-        lintCommand: `eslint ${pathResolve("../{src,mock,build}/**/*.{vue,js,ts,tsx}")}`,
+        lintCommand: `eslint ${pathResolve('../{src,mock,build}/**/*.{vue,js,ts,tsx}')}`,
         useFlatConfig: true
       },
       terminal: false,
@@ -46,7 +45,7 @@ export function getPluginsList(
     // mock支持
     vitePluginFakeServer({
       logger: false,
-      include: "mock",
+      include: 'mock',
       infixName: false,
       enableProd: true
     }),
@@ -62,10 +61,8 @@ export function getPluginsList(
     VITE_CDN ? cdn : null,
     configCompressPlugin(VITE_COMPRESSION),
     // 线上环境删除console
-    removeConsole({ external: ["src/assets/iconfont/iconfont.js"] }),
+    removeConsole({ external: ['src/assets/iconfont/iconfont.js'] }),
     // 打包分析
-    lifecycle === "report"
-      ? visualizer({ open: true, brotliSize: true, filename: "report.html" })
-      : (null as any)
-  ];
+    lifecycle === 'report' ? visualizer({ open: true, brotliSize: true, filename: 'report.html' }) : (null as any)
+  ]
 }
